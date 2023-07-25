4ca4facb97e274383da1e4acad98cb2c1739080e
root <Phindileayandasibiya6@gmail.com>
2023-07-25 12:39:29 -0700

    Everything can be imported

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#Write a program that imports the variable a from the file variable_load_2.py and prints its value.
+if __name__ == "__main__":
+    from variable_load_2 import a
+    print("{}".format(a))
