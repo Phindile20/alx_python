bacccdb9b8511e511eddda96af7c3c35cdaab776
root <Phindileayandasibiya6@gmail.com>
2023-08-10 10:54:46 -0700

    Search API

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,22 @@
+"""
+Write a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+url = "http://0.0.0.0:5000/search_user"
+letter = sys.argv[1] if len(sys.argv) > 1 else ""
+
+data = {'q': letter}
+response = requests.post(url, data=data)
+
+try:
+    json_data = response.json()
+    if json_data:
+        print("[{}] {}".format(json_data['id'], json_data['name']))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
