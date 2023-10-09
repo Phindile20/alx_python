062430a0732809bf66ec4e6cc99191987c40d16c
Phindile Ayanda Sibiya <126613368+Phindile20@users.noreply.github.com>
2023-10-09 21:25:21 +0200

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
+for tens_digit in range(10):
+    for ones_digit in range(tens_digit + 1, 10):
+        print(
+            "{:d}{:d}".format(tens_digit, ones_digit),
+            end=", " if tens_digit < 8 or ones_digit < 9 else "\n",
+        )
