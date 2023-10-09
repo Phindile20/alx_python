2386603aad4187ec72b8d54694cff36bd769eb4b
Phindile Ayanda Sibiya <126613368+Phindile20@users.noreply.github.com>
2023-10-09 22:17:04 +0200

    . Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,20 +1,9 @@
-#!/usr/bin/python3
 
-"""Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
-
+#!/usr/bin/env python3
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python script_name.py <URL>")
-    sys.exit(1)
-
 url = sys.argv[1]
+
 response = requests.get(url)
-
-
-if 'X-Request-Id' in response.headers:
-    request_id = response.headers['X-Request-Id']
-    print(request_id)
-else:
-    print("X-Request-Id header not found in the response.")
+print(response.headers.get("X-Request-Id"))
